af6be32f89b9d4e033bc4725a232d9d2048a2abe
Marc Arch <darkreplicants@gmail.com>
2015-04-10 13:04:02 +0200

    New basic scripts created

---
+++ readImage.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+#
+# This script reads from a file "linux.png" an image
+# shows some images properties at command line and
+# shows image on the screen
+#
+
+from scipy import misc
+import matplotlib.pyplot as plt
+
+# Read image from file
+linuximage = misc.imread('linux.png')
+
+# Display on CMD some image properties
+print "Image type: ", type(linuximage)
+print "Image size: ",linuximage.size
+print "Image shape: ",linuximage.shape
+print "Image type: ", linuximage.dtype 
+
+# Display image on the screen
+plt.imshow(linuximage)
+plt.show()
