2e4aa075183ec65feb0147b5e13e7d6477255f82
Marc Arch <darkreplicants@gmail.com>
2015-04-11 12:59:38 +0200

    Shows image statistics and plots histogram & image

--- basics/infoImage.py
+++ basics/infoImage.py
@@ -2,7 +2,6 @@
 import scipy.misc
 import matplotlib.pyplot as plt
 
-
 lena8b = scipy.misc.lena()
 
 print "Mean pixel value: ", lena8b.mean()
@@ -10,9 +9,16 @@ print "Min pixel value: ",lena8b.min()
 print "Max pixel value: ",lena8b.max()
 
 # Plot image histogram
+plt.subplot(1,2,1)
 plt.hist(lena8b.flatten(), 100)
 plt.xlabel('Intensity')
 plt.ylabel('Number of Pixels')
-plt.title(r'Histogram')
+plt.title('Histogram')
+
+plt.subplot(1,2,2)
+plt.imshow(lena8b)
+plt.gray()
+plt.title('Image')
+
 plt.show()
 
